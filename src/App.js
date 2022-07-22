827f624b4c1d501096a2f48db641e5c82d9d3710
Juanmanuba <jmnbvm@gmail.com>
2022-07-23 01:15:51 +0400

    hero static done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/navbar.component';
+import Hero from './components/hero/hero.component';
+import Card from './components/card/card.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Card />
     </div>
   );
 }
