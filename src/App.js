260b81bd1bda32cf604a982895f41efba158ee8f
Juanmanuba <jmnbvm@gmail.com>
2022-08-02 23:44:23 +0200

    added card and cars Section component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/navbar.component';
 import Hero from './components/hero/hero.component';
-import Card from './components/card/card.component';
+import CardSection from './components/cardSection/cardSection.component';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <CardSection />
     </div>
   );
 }
